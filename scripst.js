0dcf7b300b272a1b9e71cc0c3045e91d18aeb8b7
Gabriel <oliveirafrerreira97@hotmail.com>
2025-01-19 12:28:02 -0300

    projeto quase finalizado, apenas falta a implementação do reset do jogo

--- scripst.js
+++ scripst.js
@@ -1,4 +1,10 @@
+// Criação da lista
+
 const list = [];
+
+// Captura de elementos
+
+const form = document.querySelector("form");
 
 const buttonSort = document.getElementById("sort-button");
 
@@ -8,15 +14,31 @@ const input = document.querySelector("input");
 
 const friendsList = document.querySelector("ul");
 
-function gerarAmigoAleatorio(list) {
+// Funções
 
+function gerarAmigoAleatorio(list) {
   const nomeAleatorio = list.sort(() => Math.random() - 0.5)[0];
 
   return nomeAleatorio;
-
 }
 
-button.addEventListener("click", () => {
+function adicionarAmigoAleatorioNoHtml() {
+  const p = document.createElement("p");
+  const amigoSecreto = gerarAmigoAleatorio(list);
+
+  p.textContent = `O amigo secreto sorteado é: ${amigoSecreto}`;
+  p.style.color = "red";
+  p.style.fontWeight = "bold";
+  const liToRemove = document.querySelector("ul");
+
+  liToRemove.parentNode.replaceChild(p, liToRemove);
+
+  input.addEventListener("click", () => {
+    p.parentNode.removeChild(p);
+  });
+}
+
+function adicionarAmigoNaLista() {
   const inputValue = input.value;
 
   if (inputValue) {
@@ -28,19 +50,16 @@ button.addEventListener("click", () => {
   } else {
     alert("Por favor, insira um valor antes de adicionar."); // Evitar li vazios
   }
+}
+
+// Código principal
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  adicionarAmigoNaLista();
+  
 });
 
-
 buttonSort.addEventListener("click", () => {
-  const p = document.createElement("p")
-  const amigoSecreto = gerarAmigoAleatorio(list);
-
-  p.textContent = `O amigo secreto sorteado é: ${amigoSecreto}`;
-  p.style.color = "red";
-  p.style.fontWeight = "bold"
-  const liToRemove = document.querySelector("ul");
-
-  liToRemove.parentNode.replaceChild(p, liToRemove);
-
-})
-
+  adicionarAmigoAleatorioNoHtml();
+});
